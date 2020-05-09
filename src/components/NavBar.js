026510d9310a6689a303ba0a2227047cee254bd2
catherinism <catcreates13@gmail.com>
2020-05-09 19:48:49 -0400

    add navbar file that contain about and home links

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Menu, Icon } from 'semantic-ui-react'
+
+const NavBar = () => {
+    return (
+        <div>
+        <Menu icon='labeled' vertical tabular='right' fixed='top'>
+            
+            <Menu.Item>
+              <Icon name='home' color='blue' />
+              <Link to = '/'>Home</Link>
+              </Menu.Item>
+              
+              <Menu.Item>
+              <Icon name='lightbulb' color='blue' />
+              <Link to = '/about'>About</Link>
+              </Menu.Item>
+              
+              </Menu>
+        </div>
+    )
+}
+
+export default NavBar
