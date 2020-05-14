a9015bb51a4105c5ce75394f23b119704a807fc6
catherinism <catcreates13@gmail.com>
2020-05-14 16:00:13 -0400

    wrap app in router and import moods reducer

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
+import {BrowserRouter as Router} from 'react-router-dom'
+import moodsReducer from './reducers/moodsReducer'
 
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(moodsReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
 <Provider store={store}>
+  <Router>
   <App />
+  </Router>
 </Provider>
 , 
 document.getElementById('root'));
