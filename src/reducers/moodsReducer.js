ee245a1942a004785569beea5bcf52fb64a27e42
catherinism <catcreates13@gmail.com>
2020-05-15 15:02:02 -0400

    add action for moods and happenings

--- src/reducers/moodsReducer.js
+++ src/reducers/moodsReducer.js
@@ -3,6 +3,17 @@ export default function moodsReducer(state = {moods: []}, action) {
     switch (action.type) {
         case 'FETCH_MOODS':
             return {moods: action.payload}
+            case 'ADD_MOODS':
+                return{...state, moods: [...state.moods, action.payload]}
+                case 'ADD_HAPPENINGS':
+                    let moods = state.moods.map(mood => {
+                        if (mood.id === action.payload.id) {
+                            return action.payload
+                        } else {
+                            return mood
+                        }
+                    })
+                    return {...state, moods: moods}
             default:
                 return state
     }
