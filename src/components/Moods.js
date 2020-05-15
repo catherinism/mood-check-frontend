0a32d74ed8c94346b3c1dcb277a64d78a8f0091b
catherinism <catcreates13@gmail.com>
2020-05-15 15:05:05 -0400

    make changes to navbar icons

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -1,26 +1,43 @@
+// import React from 'react'
+// import {Link} from 'react-router-dom'
+// // import Mood from './Mood'
+
+
+// const Moods = (props) => {
+//     console.log(props)
+
+//     return (
+//         <div>   
+//             {props.moods.map(mood => 
+//             <li key={mood.id}>
+//           <Link to={`/moods/${mood.id}`}>{mood.date} - {mood.feeling}</Link>
+//         </li> )}
+//         </div>
+//     )
+// }
+
+// export default Moods
+
 import React from 'react'
-import {Route, Link} from 'react-router-dom'
-import Mood from './Mood'
+import {Link} from 'react-router-dom'
+// import Mood from './Mood'
 
 
-const Moods = ({moods}) => {
-    console.log(moods)
+class Moods extends React.Component {
+    // console.log(props)
+
+    render() {
 
     return (
         <div>   
-            {/* {props.moods.map(mood => 
+            {this.props.moods.map(mood => 
             <li key={mood.id}>
-                <Link to={`/moods/${mood.id}`}>{mood.feeling}</Link> 
-                
-            </li> )} */}
-
-            {moods && moods.map(mood => {
-                return (
-                    <Mood mood={mood} key={mood.id} />
-                )
-            })}
+          <Link to={`/moods/${this.mood.id}`}>{this.mood.date} - {this.mood.feeling}</Link>
+        </li> )}
         </div>
     )
 }
 
+}
 export default Moods
+
