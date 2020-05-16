f1d04309bae6a94dea06ab90af939c0c8e03ebfd
catherinism <catcreates13@gmail.com>
2020-05-16 14:39:10 -0400

    add styling and more definition to about page

--- src/components/About.js
+++ src/components/About.js
@@ -3,10 +3,13 @@ import React, { Component } from 'react'
 export default class About extends Component {
     render() {
         return (
+        
             <div>
 
                 <h2>
-                Mood Check is an app that tracks your mood any time.
+                Mood Check is an app that tracks your mood.
+                <br></br>
+                You can also track the weather and happenings that affected your mood.
                 </h2>
                 
             </div>
