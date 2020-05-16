ca4183bad5a26cd548d360e37ba331f0be29eb09
catherinism <catcreates13@gmail.com>
2020-05-16 14:42:03 -0400

    clean up clode

--- src/components/Happenings.js
+++ src/components/Happenings.js
@@ -1,17 +1,18 @@
 import React from 'react'
 
 const Happenings = (props) => {
+    
     console.log(props)
-
+    
     return (
+    
         <div>
+            
             {props.happenings && props.happenings.map(happening =>
-            <li key={happening.id}> <i className={`far fa-2x ${happening.name}`}></i> {happening.name}</li>
-            
-            
+            <i key={happening.id} className={`fas fa-2x ${happening.name}`}></i> 
             )}
             
-        </div>
+            </div>
         )
 }
 
