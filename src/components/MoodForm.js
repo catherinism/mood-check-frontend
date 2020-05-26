e8adc3e98df88d6aeeca084d5b8f336fe357d715
catherinism <catcreates13@gmail.com>
2020-05-26 16:41:16 -0400

    edit form and apply new actions

--- src/components/MoodForm.js
+++ src/components/MoodForm.js
@@ -1,111 +1,131 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {addMood} from '../actions/addMood'
+import {updatedForm} from '../actions/moodFormData'
+import {createMood} from '../actions/addMood'
 import { List, Form, TextArea, Button, Icon } from 'semantic-ui-react'
-
 
 class MoodForm extends Component {
 
-    state = {
-        feeling:'',
-        date:'',
-        note:'',
-        weather:''
-    }
+    // state = {
+    //     feeling:'',
+    //     date:'',
+    //     note:'',
+    //     weather:''
+    // }
 
     handleChange = (event) => {
         // debugger;
-        this.setState({
-            [event.target.name]: event.target.value
+        const {name, value} = event.target
+        const currentFormData = Object.assign({}, this.props.moodFormData, {
+            [name]: value
         })
+        this.props.updatedForm(currentFormData)
+        // this.setState({
+        //     [event.target.name]: event.target.value
+        // })
     }
     
-    handleItemClick = (e, data) => 
-    this.setState({ 
-        [data.name]: data.value 
-    });
+    // handleItemClick = (e, data) => 
+    //     // console.log(data)
+    // // this.setState({ 
+    // //     [data.name]: data.value 
+    // // })
 
-    handleSubmit = (event) => {
+
+     handleSubmit = (event) => {
         event.preventDefault()
-        this.props.addMood(this.state, this.props.history)
+
+        const {createMood, moodFormData, history} = this.props
+        createMood(moodFormData, history)
     }
-    
 
     render() {
-
+        const { feeling, date, weather, note } = this.props.moodFormData;
+       
         return (
            
             <div>
 
                 <Form widths='equal' size="big" onSubmit={this.handleSubmit}>
-                <Form.Group><Form.Field required>
+                <Form.Group><Form.Field>
 
-                <Form.Input 
+                <Form.Input
                 label='Today is' 
                 type='text' 
                 name="date" 
                 placeholder="May 1, 2020"
-                value={this.state.date} 
-                required
+                value={date} 
                 onChange={this.handleChange}/>
                 
                 <br></br><br></br>
 
                 <label>I am feeling</label>
-                
+               
                 <List horizontal relaxed size='large' selection>
-                    
-                    <List.Item  
+
+                    <List.Item>
+                    <input className="ui radio checkbox space"
+                    type="radio"
                     name="feeling"
                     value="fa-smile"
-                    active={this.state.feeling === "fa-smile" ? true : false}
-                    onClick={this.handleItemClick}>
+                    checked={feeling === "fa-smile" ? true : false}
+                    onChange={this.handleChange}/>
                     <label><i className="far fa-2x fa-smile"></i></label>
                     <List.Content><List.Header as='a'>Happy</List.Header></List.Content>
                     </List.Item>
                     
-                    <List.Item
+                    <List.Item>
+                    <input className="ui radio checkbox space"
+                    type="radio"
                     name="feeling"
                     value="fa-laugh-beam"
-                    active={this.state.feeling === "fa-laugh-beam"}
-                    onClick={this.handleItemClick}>
+                    checked={feeling === "fa-laugh-beam"}
+                    onChange={this.handleChange}/>
                     <label><i className="far fa-2x fa-laugh-beam"></i></label>
                     <List.Content><List.Header as='a'>Excited</List.Header></List.Content>
                     </List.Item>
 
-                    <List.Item
+                    <List.Item>
+                    <input className="ui radio checkbox space"
+                    type="radio"
                     name="feeling"
                     value="fa-smile-wink"
-                    active={this.state.feeling === "fa-smile-wink"}
-                    onClick={this.handleItemClick}>
+                    checked={feeling === "fa-smile-wink"}
+                    onChange={this.handleChange}/>
                     <label><i className="far fa-2x fa-smile-wink"></i></label>
                     <List.Content><List.Header as='a'>Relaxed</List.Header></List.Content>
                     </List.Item>
 
-                    <List.Item
+                    <List.Item>
+                    <input className="ui radio checkbox space"
+                    type="radio"
                     name="feeling"
                     value="fa-frown"
-                    active={this.state.feeling === "fa-frown"}
-                    onClick={this.handleItemClick}>
+                    checked={feeling === "fa-frown"}
+                    onChange={this.handleChange}/>
                     <label><i className="far fa-2x fa-frown"></i></label>
                     <List.Content><List.Header as='a'>Sad</List.Header>
                     </List.Content>
                     </List.Item>
 
-                    <List.Item
+                    <List.Item>
+                    <input className="ui radio checkbox space"
+                    type="radio"
                     name="feeling"
                     value="fa-sad-cry"
-                    active={this.state.mood === "fa-sad-cry"}
-                    onClick={this.handleItemClick}>
+                    checked={feeling === "fa-sad-cry"}
+                    onChange={this.handleChange}/>
                     <label><i className="far fa-2x fa-sad-cry"></i></label>
                     <List.Content><List.Header as='a'>Worried</List.Header></List.Content>
                     </List.Item>
 
-                    <List.Item
+                    <List.Item>
+                    <input className="ui radio checkbox space"
+                    type="radio"
                     name="feeling"
                     value="fa-angry"
-                    active={this.state.mood === "fa-angry"}
-                    onClick={this.handleItemClick}>
+                    checked={feeling === "fa-angry"}
+                    onChange={this.handleChange}/>
                     <label><i className="far fa-2x fa-angry"></i></label>
                     <List.Content><List.Header as='a'>Angry</List.Header>
                     </List.Content>
@@ -118,58 +138,70 @@ class MoodForm extends Component {
                     
                     <List horizontal relaxed size='large' selection>
                         
-                        <List.Item  
+                        <List.Item>
+                        <input className="ui radio checkbox space"
+                        type="radio" 
                         name="weather"
                         value="fa-sun"
-                        active={this.state.weather === "fa-sun" ? true : false}
-                        onClick={this.handleItemClick}>
-                        <label><i className="fas fa-sun"></i></label>
+                        checked={weather === "fa-sun" ? true : false}
+                        onChange={this.handleChange}/>
+                        <label><i className="fas fa-2x fa-sun"></i></label>
                         <List.Content><List.Header as='a'>Sunny</List.Header></List.Content>
                         </List.Item>
                 
-                        <List.Item
+                        <List.Item>
+                        <input className="ui radio checkbox space"
+                        type="radio"
                         name="weather"
                         value="fa-cloud"
-                        active={this.state.weather === "fa-cloud"}
-                        onClick={this.handleItemClick}>
-                        <label><i className="fas fa-cloud"></i></label>
+                        checked={weather === "fa-cloud"}
+                        onChange={this.handleChange}/>
+                        <label><i className="fas fa-2x fa-cloud"></i></label>
                         <List.Content><List.Header as='a'>Cloudy</List.Header></List.Content>
                         </List.Item>
 
-                        <List.Item
+                        <List.Item>
+                        <input className="ui radio checkbox space"
+                        type="radio"
                         name="weather"
                         value="fa-umbrella"
-                        active={this.state.weather === "fa-umbrella"}
-                        onClick={this.handleItemClick}>
-                        <label><i className="fas fa-umbrella"></i></label>
+                        checked={weather === "fa-umbrella"}
+                        onChange={this.handleChange}/>
+                        <label><i className="fas fa-2x fa-umbrella"></i></label>
                         <List.Content><List.Header as='a'>Rainy</List.Header></List.Content>
                         </List.Item>
 
-                        <List.Item
+                        <List.Item>
+                        <input className="ui radio checkbox space"
+                        type="radio"
                         name="weather"
                         value="fa-snowflake"
-                        active={this.state.weather === "fa-snowflake"}
-                        onClick={this.handleItemClick}>
-                        <label><i className="fas fa-snowflake"></i></label>
+                        checked={weather === "fa-snowflake"}
+                        onChange={this.handleChange}/>
+                        <label><i className="fas fa-2x fa-snowflake"></i></label>
                         <List.Content><List.Header as='a'>Snowing</List.Header>
                         </List.Content>
                         </List.Item>
 
-                        <List.Item
+                        <List.Item>
+                        <input className="ui radio checkbox space"
+                        type="radio"
                         name="weather"
                         value="fa-temperature-low"
-                        active={this.state.weather === "fa-temperature-low"}
-                        onClick={this.handleItemClick}>
-                        <label><i className="fas fa-temperature-low"></i></label>
+                        checked={weather === "fa-temperature-low"}
+                        onChange={this.handleChange}/>
+                        <label><i className="fas fa-2x fa-temperature-low"></i></label>
                         <List.Content><List.Header as='a'>Cold</List.Header></List.Content>
                         </List.Item>
 
-                        <List.Item
+                        <List.Item>
+                        <input className="ui radio checkbox space"
+                        type="radio"
                         name="weather"
                         value="fa-thermometer-full"
-                        active={this.state.weather === "fa-thermometer-full"}
-                        onClick={this.handleItemClick}>
-                        <label><i className="fas fa-thermometer-full"></i></label>
+                        checked={weather === "fa-thermometer-full"}
+                        onChange={this.handleChange}/>
+                        <label><i className="fas fa-2x fa-thermometer-full"></i></label>
                         <List.Content><List.Header as='a'>Humid</List.Header>
                         </List.Content>
                         </List.Item>
@@ -184,7 +216,7 @@ class MoodForm extends Component {
                         name="note" 
                         placeholder='Today, I had a lovely dinner with my husband.'
                         
-                        value={this.state.note} 
+                        value={note} 
                         onChange={this.handleChange}/>
                     
                         <br></br><br></br>
@@ -200,4 +232,15 @@ class MoodForm extends Component {
                     }
                 }
 
-export default connect(null, {addMood})(MoodForm)
+const mapStateToProps = state => {
+    // console.log(state.formsReducer)
+    return ({
+        moodFormData: state.formsReducer
+
+    })
+}                 
+
+export default connect(mapStateToProps, {createMood, updatedForm})(MoodForm)
+
+
+
