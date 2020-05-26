765f7ec754dec39370749a3ed22baf94434d27a0
catherinism <catcreates13@gmail.com>
2020-05-26 16:36:26 -0400

    created action creators in addMood file

--- src/actions/addMood.js
+++ src/actions/addMood.js
@@ -1,17 +1,72 @@
-export const addMood = (data, history) => {
-    return (dispatch) => {
-        fetch('http://localhost:3000/api/v1/moods', {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            method: 'POST',
-            body: JSON.stringify(data)
-        })
-        .then(response => response.json())
-        .then(mood => { dispatch({type: 'ADD_MOODS', payload: mood})
-        history.push(`/moods/${mood.id}`) 
-        })
+export const updatedForm = moodFormData => {
+    return {
+        type: 'UPDATED_FORM',
+        moodFormData
     }
 }
+
+export const resetForm = () => {
+  return {
+      type: 'RESET_FORM'
+  }
+}
+
+const addMood = mood => {
+    return {
+        type: 'CREATE_MOOD',
+        mood
+    }
+}
+
+export const setMoods = moods => {
+    return {
+      type: 'GET_MOODS',
+      moods
+    }
+  }
+    
+    export const getMoods = () => {
+    //   console.log("c")
+      return dispatch => {
+        //   console.log('inside fetch moods')
+      return fetch('http://localhost:3000/api/v1/moods')
+        .then(response => response.json())
+        .then(moods => dispatch(setMoods(moods)))
+           .catch(error => console.log(error))
+      }
+  // console.log("e")
+  }
   
+  export const fetchMood = (moodId) => {
+      
+    return dispatch => {
+      return fetch(`http://localhost:3000/api/v1/moods/${moodId}`)
+      .then(response => response.json())
+      .then(mood => {
+        dispatch(setMoods([mood]))
+      })
+    }
+  }
+
+export const createMood = (mood, routerHistory) => {
+    console.log("im here")
+
+    return dispatch => {
+        return fetch('http://localhost:3000/api/v1/moods', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                // 'Accept': 'application/json'
+            },
+            body: JSON.stringify({mood: mood})
+        })
+        .then(response => response.json())
+        .then(mood => {
+            // dispatch({type: 'ADD_MOODS', payload: mood})
+            dispatch(addMood(mood))
+            dispatch(resetForm())
+            routerHistory.replace(`/moods/${mood.id}`)   
+        })
+        .catch(error => console.log(error))
+    }
+}
--- src/actions/fetchMoods.js
+++ src/actions/fetchMoods.js
@@ -1,16 +1,30 @@
-export default function fetchMoods() {
-    console.log("c")
-    return (dispatch) => {
-        // console.log('inside fetch moods')
-    fetch('http://localhost:3000/api/v1/moods')
-      .then(response => response.json())
-      .then(moods => {
-          console.log("d");
-          dispatch({
-          type: 'FETCH_MOODS',
-          payload: moods
-        })
-    })
-    }
-console.log("e")
-}
+// export const setMoods = moods => {
+//   return {
+//     type: 'GET_MOODS',
+//     moods
+//   }
+// }
+  
+//   export const fetchMoods = () => {
+//     console.log("c")
+//     return dispatch => {
+//         // console.log('inside fetch moods')
+//     return fetch('http://localhost:3000/api/v1/moods')
+//       .then(response => response.json())
+//       .then(moods => 
+//         //   console.log("d");
+//          dispatch(setMoods(moods)))
+//          .catch(error => console.log(error))
+//     }
+// // console.log("e")
+// }
+
+// export const getMood = (moodId) => {
+//   return dispatch => {
+//     return fetch(`http://localhost:3000/api/v1/moods/${moodId}`)
+//     .then(response => response.json())
+//     .then(mood => {
+//       dispatch(setMoods([mood]))
+//     })
+//   }
+// }
