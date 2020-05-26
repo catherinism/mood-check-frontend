f492291d68c1c5c39ab3e3c9b7be168c5d265fa4
catherinism <catcreates13@gmail.com>
2020-05-26 16:38:54 -0400

    remove routes and map through each mood

--- src/containers/MoodsContainer.js
+++ src/containers/MoodsContainer.js
@@ -1,50 +1,32 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
-import fetchMoods from '../actions/fetchMoods'
-import Moods from '../components/Moods'
-import Mood from '../components/Mood'
-import MoodForm from '../components/MoodForm'
-import {Route, Switch} from 'react-router-dom'
-import Home from '../components/Home'
-import About from '../components/About'
+import {getMoods} from '../actions/addMood'
+import MoodCard from '../components/MoodCard'
 
 class moodsContainer extends Component {
 
     componentDidMount() {
-        console.log("a")
-        this.props.fetchMoods()
-        console.log("b")
+        // console.log("a")
+        this.props.getMoods()
     }
 
     render() {
-
+        
         return (
             <div>
-                <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route path='/moods/new' component={MoodForm} />
-                <Route path='/moods/:id' render={(routerProps) => <Mood {...routerProps} moods={this.props.moods}/>} />
-                <Route exact path='/moods' render={() => <Moods moods={this.props.moods} />} />
-                </Switch>
-
-                <ul>
                 
-                    <br></br>
-                    <br></br>
-                </ul>
+            {this.props.moods.map(mood => <MoodCard key={mood.id} mood={mood} />)}
+
             </div>
         )
     }
 }
 
-const mapStateToProps = state => {
-    return {
-        moods: state.moods
-    }
-}
-
-export default connect(
-    mapStateToProps,
-    {fetchMoods}
-    )(moodsContainer)
+const mapStateToProps = (state) => {
+    // console.log(state.moodsReducer)
+    return ({
+      moods: state.moodsReducer
+    })
+  }
+  
+  export default connect(mapStateToProps, { getMoods })(moodsContainer)
