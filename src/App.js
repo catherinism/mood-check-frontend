e18e954d1ab28b25f7bd7ef4f86c25f762fe9eee
catherinism <catcreates13@gmail.com>
2020-05-26 16:34:18 -0400

    move routes to app file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import MoodsContainer from './containers/MoodsContainer'
 import NavBar from './components/NavBar'
+import Home from './components/Home'
+import About from './components/About'
+// import Moods from './components/Moods'
+import Mood from './components/Mood'
+import MoodForm from './components/MoodForm'
 
 class App extends React.Component {
   
@@ -9,11 +15,20 @@ class App extends React.Component {
     return (
 
     <div className="App">
-
+    <Router>
+      
       <NavBar />
-      <MoodsContainer />
-    
+      <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path='/moods' component={MoodsContainer} />
+                <Route exact path='/moods/new' component={MoodForm} />
+                <Route exact path='/moods/:moodId' component={Mood} />
+                </Switch>
+
+                </Router>
     </div>
+    
   );
 }
 
